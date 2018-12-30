e0dbc9d89c9dd518c35551664ab35a2d46f4873e
Steel Brain <i@steelbrain.me>
2018-12-31 00:36:57 +0500

    :arrow_up: Upgrade dependencies

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,42 +1,38 @@
 'use babel'
 
-import jsxString from '../'
+import jsxString from '..'
 
 /** @jsx jsxString.h */
 
 describe('JSX String', function() {
   it('works well', function() {
-    expect(jsxString(
-      <div><span>
-        {'Hello World'}
-      </span></div>,
-    )).toBe('<div><span>Hello World</span></div>')
+    expect(
+      jsxString(
+        <div>
+          <span>Hello World</span>
+        </div>,
+      ),
+    ).toBe('<div><span>Hello World</span></div>')
   })
   it('escapes bodies properly', function() {
-    expect(jsxString(
-      <div>{'<script />'}</div>,
-    )).toBe('<div>&lt;script /&gt;</div>')
+    expect(jsxString(<div>{'<script />'}</div>)).toBe('<div>&lt;script /&gt;</div>')
   })
   it('supports attributes', function() {
-    expect(jsxString(
-      <div a="b">
-        <span c_d="e">Hey</span>Man
-      </div>,
-    )).toBe('<div a="b"><span c:d="e">Hey</span>Man</div>')
+    expect(
+      jsxString(
+        <div a="b">
+          <span c_d="e">Hey</span>Man
+        </div>,
+      ),
+    ).toBe('<div a="b"><span c:d="e">Hey</span>Man</div>')
   })
   it('supports attributes with more than one colon in self closing tags', function() {
-    expect(jsxString(
-      <div a_b_c="d" />,
-    )).toBe('<div a:b:c="d" />')
+    expect(jsxString(<div a_b_c="d" />)).toBe('<div a:b:c="d" />')
   })
   it('supports escaping in attributes', function() {
-    expect(jsxString(
-      <div a={'<script "" />'} />,
-    )).toBe('<div a="&lt;script &quot;&quot; /&gt;" />')
+    expect(jsxString(<div a={'<script "" />'} />)).toBe('<div a="&lt;script &quot;&quot; /&gt;" />')
   })
   it('supports disabling escaping', function() {
-    expect(jsxString(
-      <div>{'<script />'}</div>, { escape: false },
-    )).toBe('<div><script /></div>')
+    expect(jsxString(<div>{'<script />'}</div>, { escape: false })).toBe('<div><script /></div>')
   })
 })
